c53b5761cb74f3dda9f4fbbd1e16fd66774741ec
Mauricio jr <juniorcavalcanti@live.com>
2024-07-14 17:50:58 -0300

    ajuste de contesxto para login

--- cypress/e2e/tests/prd/login.cy.js
+++ cypress/e2e/tests/prd/login.cy.js
@@ -13,15 +13,15 @@ describe('Login', () => {
             cy.url().should('eq', url);
 
             cy.wait(5000)
-        })
-        it('login com dados validos', () => {
+
             //Inserindo credencias para realizar login
             cy.get(loginElements.campoLogin).type(globalData.usuarioValido.email)
             cy.get(loginElements.campoSenha).type(globalData.usuarioValido.password)
-
+    
             // click para entrar na conta do usuario
             cy.get(loginElements.btnEntrar).should('be.visible').click()
-
+        })
+        it('login com dados validos', () => {
             // Aguardando o texto estar visivel para fazer a validação correta do elemento
             cy.contains(homeElements.elementNameUser, 'Olá, Automation!', { timeout: 10000 }) // 10 segundos
               .should('be.visible');
@@ -31,14 +31,7 @@ describe('Login', () => {
                 .should('be.visible')
                 .and('have.text', 'Olá, Automation!');
         });
-        it.only('Logout', () => {
-            //Inserindo credencias para realizar login
-            cy.get(loginElements.campoLogin).type(globalData.usuarioValido.email)
-            cy.get(loginElements.campoSenha).type(globalData.usuarioValido.password)
-
-            // click para entrar na conta do usuario
-            cy.get(loginElements.btnEntrar).should('be.visible').click()
-
+        it('Logout', () => {
             // Aguardando o texto estar visivel para fazer a validação correta do elemento
             cy.contains(homeElements.elementNameUser, 'Olá, Automation!', { timeout: 10000 }) // 10 segundos
                 .should('be.visible');
@@ -65,4 +58,28 @@ describe('Login', () => {
                 .and('have.text', 'Minha conta');
         });
     })
+    it('login com dados invalidos', () => {
+        const url = 'https://www.avon.com.br/login?redirect=%2F';
+            // Usa a variável para visitar a URL
+            cy.visit(url);
+            // verifica que estamos na Url correta  
+            cy.url().should('eq', url);
+
+            cy.wait(5000)
+        //Inserindo credencias invalidas para realizar login
+        cy.get(loginElements.campoLogin).type(globalData.usuarioInexistente.email)
+        cy.get(loginElements.campoSenha).type(globalData.usuarioInexistente.password)
+
+        // click para entrar na conta do usuario
+        cy.get(loginElements.btnEntrar).should('be.visible').click()
+
+        // Aguardando o texto estar visivel para fazer a validação correta do elemento
+        cy.contains(loginElements.textLoginInvalid, 'Informe um login válido', { timeout: 10000 }) // 10 segundos
+            .should('be.visible');
+
+        // Valida que realizei o login com sucesso 
+        cy.get(loginElements.textLoginInvalid2)
+            .should('exist')
+            .and('have.text', globalAssertion.loginAssertion.textMessageLoginInvalid);
+    });
 })
